95f1a932009fbdf7d703d0e38d0a216d5c096264
Andrea Filomena <andrea.filomena@icsone.it>
2020-06-21 14:36:48 +0200

    aggiornate configurazioni d'accesso

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'eu-central-1_4J8DpDD3v', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '6q1cva45esciah6u3br6kg7026', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'eu-centra-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
