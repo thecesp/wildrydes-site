d934fe58b41585efb2ffc9c060dde24b56a93080
Andrea Filomena <andrea.filomena@icsone.it>
2020-06-23 14:45:59 +0200

    configurazione api

--- js/config.js
+++ js/config.js
@@ -5,6 +5,6 @@ window._config = {
         region: 'eu-centra-1' // e.g. us-east-2
     },
     api: {
-        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: 'https://jvbedxwl22.execute-api.eu-central-1.amazonaws.com/prod',
     }
 };
